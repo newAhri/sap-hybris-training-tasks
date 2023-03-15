6b403b44837859ebf024f4e079db8f39db50c606
newAhri <gagotun5@gmail.com>
2023-03-15 15:47:30 +0200

    added prepare interceptor

--- training/trainingcore/src/org/training/core/interceptors/EcentaNotificationPrepareInterceptor.java
+++ training/trainingcore/src/org/training/core/interceptors/EcentaNotificationPrepareInterceptor.java
@@ -2,11 +2,23 @@ package org.training.core.interceptors;
 
 import de.hybris.platform.servicelayer.interceptor.InterceptorContext;
 import de.hybris.platform.servicelayer.interceptor.InterceptorException;
+import de.hybris.platform.servicelayer.interceptor.PersistenceOperation;
 import de.hybris.platform.servicelayer.interceptor.PrepareInterceptor;
+import org.training.core.model.EcentaNotificationModel;
 
 public class EcentaNotificationPrepareInterceptor implements PrepareInterceptor {
     @Override
     public void onPrepare(Object o, InterceptorContext interceptorContext) throws InterceptorException {
-
+        if (o instanceof EcentaNotificationModel) {
+            EcentaNotificationModel model = (EcentaNotificationModel) o;
+            if (model.getRead()) {
+                model.setDeleted(true);
+            } else if (!model.getRead()){
+                model.setDeleted(false);
+            } else {
+                model.setDeleted(null);
+            }
+            interceptorContext.registerElementFor(model, PersistenceOperation.SAVE);
+        }
     }
 }
