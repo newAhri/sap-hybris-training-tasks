449fb56169004d4db43687d9c9a22c80089f977d
newAhri <gagotun5@gmail.com>
2023-03-15 13:19:06 +0200

    interceptor bean added; before EN type id change to String

---
+++ training/trainingcore/src/org/training/core/interceptors/EcentaNotificationInitDefaultsInterceptor.java
@@ -0,0 +1,28 @@
+package org.training.core.interceptors;
+
+import de.hybris.platform.servicelayer.interceptor.InitDefaultsInterceptor;
+import de.hybris.platform.servicelayer.interceptor.InterceptorContext;
+import de.hybris.platform.servicelayer.interceptor.InterceptorException;
+import de.hybris.platform.servicelayer.keygenerator.impl.PersistentKeyGenerator;
+import org.training.core.model.EcentaNotificationModel;
+
+public class EcentaNotificationInitDefaultsInterceptor implements InitDefaultsInterceptor {
+    private PersistentKeyGenerator uniqueAlphanumericIDKeyGenerator;
+
+    @Override
+    public void onInitDefaults(Object o, InterceptorContext interceptorContext) throws InterceptorException {
+        if (o instanceof EcentaNotificationModel){
+            EcentaNotificationModel model = (EcentaNotificationModel) o;
+            //model.setId(uniqueAlphanumericIDKeyGenerator.generate());
+
+        }
+    }
+
+    public PersistentKeyGenerator getUniqueAlphanumericIDKeyGenerator() {
+        return uniqueAlphanumericIDKeyGenerator;
+    }
+
+    public void setUniqueAlphanumericIDKeyGenerator(PersistentKeyGenerator uniqueAlphanumericIDKeyGenerator) {
+        this.uniqueAlphanumericIDKeyGenerator = uniqueAlphanumericIDKeyGenerator;
+    }
+}
