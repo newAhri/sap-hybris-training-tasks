6e43c70760079b94f4a7888c862b12e16214d6e7
newAhri <gagotun5@gmail.com>
2023-05-16 21:37:43 +0300

    task 13 complete

--- training/trainingstorefront/web/src/org/training/storefront/controllers/pages/EcentaNotificationsPageController.java
+++ training/trainingstorefront/web/src/org/training/storefront/controllers/pages/EcentaNotificationsPageController.java
@@ -3,16 +3,12 @@ package org.training.storefront.controllers.pages;
 import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.AbstractPageController;
 import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
 import de.hybris.platform.cms2.model.pages.ContentPageModel;
-import de.hybris.platform.servicelayer.user.UserService;
+import de.hybris.platform.util.Config;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.training.core.service.EcentaNotificationFindByService;
-import org.training.facades.custom.EcentaNotificationFacade;
-
-import javax.annotation.Resource;
 
 @Controller
 @Scope("tenant")
@@ -20,16 +16,15 @@ import javax.annotation.Resource;
 public class EcentaNotificationsPageController extends AbstractPageController
 {
     private static final String ECENTA_NOTIFICATIONS_CMS_PAGE = "ecentaNotificationsCMSPage";
-    @Resource(name = "ecentaNotificationFacade")
-    private EcentaNotificationFacade ecentaNotificationFacade;
-    @Resource(name = "userService")
-    private UserService userService;
-    @Resource(name = "defaultEcentaNotificationFindByService")
-    private EcentaNotificationFindByService ecentaNotificationFindByService;
+    private static final String ECENTA_NOTIFICATIONS_SHOW_MAX = "ecenta.notifications.show.max";
+
 
     @RequestMapping(method = RequestMethod.GET)
     public String getEcentaNotifications(final Model model) throws CMSItemNotFoundException
     {
+        final int maxAmountToShow = Integer.parseInt(Config.getParameter(ECENTA_NOTIFICATIONS_SHOW_MAX)) - 1;
+        model.addAttribute("maxAmountToShow", maxAmountToShow);
+
         final ContentPageModel ecentaNotificationsCMSPage = getContentPageForLabelOrId(ECENTA_NOTIFICATIONS_CMS_PAGE);
         storeCmsPageInModel(model, ecentaNotificationsCMSPage);
         setUpMetaDataForContentPage(model, ecentaNotificationsCMSPage);
